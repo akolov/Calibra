58bdb2d57c6882a4137c58a5ce60982b3ea7d671
Alexander Kolov <me@alexkolov.com>
2015-11-15 21:34:14 +0100

    Initial Commit

---
+++ Calibra/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Calibra
+//
+//  Created by Alexander Kolov on 11/15/15.
+//  Copyright © 2015 Alexander Kolov. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: AnyObject? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
